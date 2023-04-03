e85ea1d1ba9ba669d5e5ca3e3e832d5b43033753
seitamuro <seitamuro0210@gmail.com>
2023-04-03 19:51:54 +0900

    firebaseを使う準備を整えた

--- pages/experiment/firebase/index.tsx
+++ pages/experiment/firebase/index.tsx
@@ -1,3 +1,16 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCta833k8W1TGbr9tGa60y-uOOpZihd4Zw",
+  authDomain: "seitamuro-blog3-experiment.firebaseapp.com",
+  projectId: "seitamuro-blog3-experiment",
+  storageBucket: "seitamuro-blog3-experiment.appspot.com",
+  messagingSenderId: "1061901812373",
+  appId: "1:1061901812373:web:9f2ba98b1c1b0bf7b0119c"
+};
+
+const app = initializeApp(firebaseConfig);
+
 export default function ExperimentFirebase() {
   return (
     <>
