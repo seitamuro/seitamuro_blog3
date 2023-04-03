db801a9f86130f608e191b49620cee6ec9bf6549
seitamuro <seitamuro0210@gmail.com>
2023-04-03 21:04:24 +0900

    データを取得

--- pages/experiment/firebase/index.tsx
+++ pages/experiment/firebase/index.tsx
@@ -1,4 +1,5 @@
-import { initializeApp } from "firebase/app";
+import firebase from "firebase/app";
+import "firebase/firestore"
 
 const firebaseConfig = {
   apiKey: "AIzaSyCta833k8W1TGbr9tGa60y-uOOpZihd4Zw",
@@ -9,9 +10,20 @@ const firebaseConfig = {
   appId: "1:1061901812373:web:9f2ba98b1c1b0bf7b0119c"
 };
 
-const app = initializeApp(firebaseConfig);
-
 export default function ExperimentFirebase() {
+  if (firebase.apps.length == 0) {
+    firebase.initializeApp(firebaseConfig);
+  }
+
+  const db = firebase.firestore();
+  const todosRef = db.collection("todos");
+  todosRef.doc("J5wZ91clVmDUE7Nqfbwj").get().then(doc => {
+    if (doc.exists) {
+      console.log(doc.data());
+    } else {
+      console.log("empty");
+    }
+  })
   return (
     <>
       <div>firebase example</div>
