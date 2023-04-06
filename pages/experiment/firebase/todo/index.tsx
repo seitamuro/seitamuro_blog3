c7b88111f8963d764b209d2de64cdce86fc9048a
seitamuro <seitamuro0210@gmail.com>
2023-04-07 00:08:33 +0900

    タスク削除機能を追加

--- pages/experiment/firebase/todo/index.tsx
+++ pages/experiment/firebase/todo/index.tsx
@@ -5,6 +5,12 @@ import firebase from "firebase/app"
 import { Task, TaskStatus } from "@/types/todo";
 import type { MyFirebaseRef } from "@/lib/firestore";
 
+/**
+ * タスクを追加する。
+ * 
+ * @param ref collectionへの参照
+ * @param title タスクのタイトル
+ */
 const addTask = (
   ref: MyFirebaseRef,
   title: string,
@@ -15,6 +21,19 @@ const addTask = (
     timestamp: firebase.firestore.Timestamp.now()
   }
   addData(ref, task);
+}
+
+/**
+ * 指定されたdocumentIdのタスクを削除する
+ * 
+ * @param ref collectionへの参照
+ * @param documentId ドキュメントID
+ */
+const deleteTask = async (
+  ref: MyFirebaseRef,
+  documentId: string,
+) => {
+  await ref.doc(documentId).delete()
 }
 
 export default function ExperimentFirebase() {
@@ -34,7 +53,7 @@ export default function ExperimentFirebase() {
   return (
     <>
       <div>firebase todo</div>
-      <div>{todos.map((todo: any) => (<div key={todo.id}>{todo.title}|{todo.status}</div>))}</div>
+      <div>{todos.map((todo: any) => (<div key={todo.id}>{todo.title}|{todo.status}<button onClick={() => deleteTask(todosRef, todo.id)}>delete</button></div>))}</div>
       <input ref={taskTitleRef}></input>
       <button onClick={() => addTask(todosRef, taskTitleRef.current!.value)}>Add</button>
     </>
